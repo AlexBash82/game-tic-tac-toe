cacc380739ce3b2d6df92ec2e8a79a48690e781a
AlexBash82 <kometa.4m@gmail.com>
2023-06-14 23:02:55 -0700

    настроил отображение выйгрышных клеток

--- src/Pages/Main/Main.tsx
+++ src/Pages/Main/Main.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useEffect} from 'react'
 import { useAppSelector, useAppDispatch } from '../../hook'
 import { moveTo } from '../../store/fieldSlice'
-import { changeTern } from '../../store/gameSlice'
+import { changeTern, makeOver, setWinnerRow } from '../../store/gameSlice'
 import { useCheckWinner } from '../../checkWinner'
 import './Main.scss'
 
@@ -10,14 +10,18 @@ export const Main: React.FC = () => {
   const dispatch = useAppDispatch()
   const { one, two, three, four, five, six, seven, eight, nine } = useAppSelector(state => state.field.gameField)
   const nowTern = useAppSelector(state => state.game.nowTern)
-  
+  const isOver = useAppSelector(state => state.game.isOver)
+  const winnerRow = useAppSelector(state => state.game.winnerRow)
 
-  const result = useCheckWinner()  
+  const result = useCheckWinner()
 
-  if(result) {
-    console.log('use WINNER - ', result)
-  }
-
+  useEffect(() => {
+    if(result) {
+      dispatch(makeOver(true))
+      dispatch(setWinnerRow(result))
+      console.log('use WINNER - ', result)
+    }
+  }, [result, dispatch])
 
   function playerMove(obj: object) {
     dispatch(moveTo(obj))
@@ -28,62 +32,65 @@ export const Main: React.FC = () => {
   return (
     <div className='Page'>
       <div className='Page_topic'>Крестики - нолики</div>
-      <div className='Page_description'>Сейчас ходят '{nowTern}'</div>
+      {isOver ? <div className='Page_description'>ПОЗДРАВЛЯЕМ!!!</div> :
+        <div className='Page_description'>Сейчас ходят '{nowTern}'</div>
+      }
+
       <button className='ChoisenBtn'>Играть ноликами</button>
       <button className='ChoisenBtn'>Начать заново</button>
       <div className='Page_field'>
         <div className='Page_field_one Item' onClick={() => playerMove({one: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={one.disable}
+            className={`Btn ${winnerRow.find(item => item === 1) ? 'Winner' : ''}`}
+            disabled={isOver || one.disable}
           >{one.sign}</button>
         </div>
         <div className='Page_field_two Item' onClick={() => playerMove({two: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={two.disable}
+            className={`Btn ${winnerRow.find(item => item === 2) ? 'Winner' : ''}`}
+            disabled={isOver || two.disable}
           >{two.sign}</button>
         </div>
         <div className='Page_field_three Item' onClick={() => playerMove({three: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={three.disable}
+            className={`Btn ${winnerRow.find(item => item === 3) ? 'Winner' : ''}`}
+            disabled={isOver || three.disable}
           >{three.sign}</button>
         </div>
         <div className='Page_field_four Item' onClick={() => playerMove({four: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={four.disable}
+            className={`Btn ${winnerRow.find(item => item === 4) ? 'Winner' : ''}`}
+            disabled={isOver || four.disable}
           >{four.sign}</button>
         </div>
         <div className='Page_field_five Item' onClick={() => playerMove({five: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={five.disable}
+            className={`Btn ${winnerRow.find(item => item === 5) ? 'Winner' : ''}`}
+            disabled={isOver || five.disable}
           >{five.sign}</button>
         </div>
         <div className='Page_field_six Item' onClick={() => playerMove({six: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={six.disable}
+            className={`Btn ${winnerRow.find(item => item === 6) ? 'Winner' : ''}`}
+            disabled={isOver || six.disable}
           >{six.sign}</button>
         </div>
         <div className='Page_field_seven Item' onClick={() => playerMove({seven: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={seven.disable}
+            className={`Btn ${winnerRow.find(item => item === 7) ? 'Winner' : ''}`}
+            disabled={isOver || seven.disable}
           >{seven.sign}</button>
         </div>
         <div className='Page_field_eight Item' onClick={() => playerMove({eight: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={eight.disable}
+            className={`Btn ${winnerRow.find(item => item === 8) ? 'Winner' : ''}`}
+            disabled={isOver || eight.disable}
           >{eight.sign}</button>
         </div>
         <div className='Page_field_nine Item' onClick={() => playerMove({nine: {sign: nowTern, disable: true}})}>
           <button
-            className='Btn'
-            disabled={nine.disable}
+            className={`Btn ${winnerRow.find(item => item === 9) ? 'Winner' : ''}`}
+            disabled={isOver || nine.disable}
           >{nine.sign}</button>
         </div>
       </div>
--- src/checkWinner.ts
+++ src/checkWinner.ts
@@ -5,73 +5,76 @@ import { useAppSelector } from './hook'
 export const useCheckWinner = () => {
   const { one, two, three, four, five, six, seven, eight, nine } = useAppSelector(state => state.field.gameField)
   const gameField = [ one, two, three, four, five, six, seven, eight, nine ]
+  const isOver = useAppSelector(state => state.game.isOver)
 
   const nowTern = useAppSelector(state => state.game.nowTern)
   const prevTern = (nowTern === 'X') ? 'O' : 'X'
-
-  let sumField: number[] = []
   let winner: number[] | null = null
 
-  for (let i=0; i<gameField.length; i++) {      
-    if(gameField[i].sign === prevTern) {
-      sumField.push(1+i)
-    }
-  }
+  if(!isOver) {
 
-  if(sumField.filter(item => item === 1).length) {
-    if(sumField.filter(item => item === 2).length) {
-      if(sumField.filter(item => item === 3).length) {
-        winner = [1, 2, 3]
-      }
-    }
-    if(sumField.filter(item => item === 5).length) {
-      if(sumField.filter(item => item === 9).length) {
-        winner = [1, 5, 9]
-      }
-    }
-    if(sumField.filter(item => item === 4).length) {
-      if(sumField.filter(item => item === 7).length) {
-        winner = [1, 4, 7]
-      }
-    }
-  }
-
-  if(sumField.filter(item => item === 2).length) {
-    if(sumField.filter(item => item === 5).length) {
-      if(sumField.filter(item => item === 8).length) {
-        winner = [2, 5, 8]
-      }
-    }
-  }
-
-  if(sumField.filter(item => item === 3).length) {
-    if(sumField.filter(item => item === 5).length) {
-      if(sumField.filter(item => item === 7).length) {
-        winner = [3, 5, 7]
-      }
-    }
-    if(sumField.filter(item => item === 6).length) {
-      if(sumField.filter(item => item === 9).length) {
-        winner = [3, 6, 9]
-      }
-    }
-  }
+    let sumField: number[] = []
   
-  if(sumField.filter(item => item === 4).length) {
-    if(sumField.filter(item => item === 5).length) {
+    for (let i=0; i<gameField.length; i++) {      
+      if(gameField[i].sign === prevTern) {
+        sumField.push(1+i)
+      }
+    }
+  
+    if(sumField.filter(item => item === 1).length) {
+      if(sumField.filter(item => item === 2).length) {
+        if(sumField.filter(item => item === 3).length) {
+          winner = [1, 2, 3]
+        }
+      }
+      if(sumField.filter(item => item === 5).length) {
+        if(sumField.filter(item => item === 9).length) {
+          winner = [1, 5, 9]
+        }
+      }
+      if(sumField.filter(item => item === 4).length) {
+        if(sumField.filter(item => item === 7).length) {
+          winner = [1, 4, 7]
+        }
+      }
+    }
+  
+    if(sumField.filter(item => item === 2).length) {
+      if(sumField.filter(item => item === 5).length) {
+        if(sumField.filter(item => item === 8).length) {
+          winner = [2, 5, 8]
+        }
+      }
+    }
+  
+    if(sumField.filter(item => item === 3).length) {
+      if(sumField.filter(item => item === 5).length) {
+        if(sumField.filter(item => item === 7).length) {
+          winner = [3, 5, 7]
+        }
+      }
       if(sumField.filter(item => item === 6).length) {
-        winner = [4, 5, 6]
+        if(sumField.filter(item => item === 9).length) {
+          winner = [3, 6, 9]
+        }
+      }
+    }
+    
+    if(sumField.filter(item => item === 4).length) {
+      if(sumField.filter(item => item === 5).length) {
+        if(sumField.filter(item => item === 6).length) {
+          winner = [4, 5, 6]
+        }
+      }
+    }
+  
+    if(sumField.filter(item => item === 7).length) {
+      if(sumField.filter(item => item === 8).length) {
+        if(sumField.filter(item => item === 9).length) {
+          winner = [7, 8, 9]
+        }
       }
     }
   }
-
-  if(sumField.filter(item => item === 7).length) {
-    if(sumField.filter(item => item === 8).length) {
-      if(sumField.filter(item => item === 9).length) {
-        winner = [7, 8, 9]
-      }
-    }
-  }
-
   return (winner)
 }
